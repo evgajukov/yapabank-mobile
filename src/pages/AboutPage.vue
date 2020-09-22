607cdf46faae87dfdb6fcd862bc9b5e4ef03cac2
Timur Evgazhukov <evgajukov@gmail.com>
2020-09-22 20:54:51 +0300

    Создана страница о проекте, а также доработан общий дизайн (добавлен логотип в шапку)

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,9 +1,19 @@
 <template>
-  <v-container fluid>about</v-container>
+  <v-container fluid>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img :src="require('@/assets/acra_blue.png')" class="my-3" contain height="200" />
+      </v-col>
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">ARCA кошелек</h1>
+        <p class="subheading font-weight-regular">Версия 1.0.0</p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: "AboutPage",
-}
+};
 </script>
--- src/pages/layers/HeaderLayer.vue
+++ src/pages/layers/HeaderLayer.vue
@@ -1,5 +1,21 @@
 <template>
   <v-app-bar app color="primary" dark>
+    <div class="d-flex align-center">
+      <v-img
+        class="shrink mr-2"
+        contain
+        :src="require('@/assets/acra_white.png')"
+        transition="scale-transition"
+        width="30"
+      />
+      <v-img
+        class="shrink mt-1 hidden-sm-and-down"
+        contain
+        min-width="100"
+        :src="require('@/assets/acra_white.png')"
+        width="100"
+      />
+    </div>
     <v-toolbar-title>{{appName}}</v-toolbar-title>
     <v-spacer></v-spacer>
     <MenuLayer />
@@ -17,6 +33,6 @@ export default {
   },
   components: {
     MenuLayer,
-  }
-}
+  },
+};
 </script>
