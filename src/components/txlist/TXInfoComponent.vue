fcb4883a14e675bb408d0bf3abea3dd53ea96a53
Timur Evgazhukov <evgajukov@gmail.com>
2020-10-11 14:47:47 +0300

    доработана поддержка отображения транзакции по созданию токена

--- src/components/txlist/TXInfoComponent.vue
+++ src/components/txlist/TXInfoComponent.vue
@@ -3,38 +3,57 @@
     <v-list>
       <v-list-item two-line>
         <v-list-item-icon>
-          <v-icon v-if="tx.type == 4 && tx.sender == wallet.address" color="red">mdi-arrow-bottom-right</v-icon>
-          <v-icon v-if="tx.type == 4 && tx.sender != wallet.address" color="success">mdi-arrow-top-right</v-icon>
-          <v-icon v-if="tx.type == 3" color="success">mdi-arrow-expand-right</v-icon>
+          <v-icon v-if="tx.type == 4 && tx.sender == wallet.address" color="red"
+            >mdi-arrow-bottom-right</v-icon
+          >
+          <v-icon
+            v-if="tx.type == 4 && tx.sender != wallet.address"
+            color="success"
+            >mdi-arrow-top-right</v-icon
+          >
+          <v-icon v-if="tx.type == 3" color="success"
+            >mdi-arrow-expand-right</v-icon
+          >
         </v-list-item-icon>
         <v-list-item-content>
-          <v-list-item-title>{{tx | typeFormat(wallet)}}</v-list-item-title>
-          <v-list-item-subtitle>{{tx | amountFormat(wallet)}} {{asset.name}}</v-list-item-subtitle>
+          <v-list-item-title>{{ tx | typeFormat(wallet) }}</v-list-item-title>
+          <v-list-item-subtitle
+            >{{ tx | amountFormat(wallet) }}
+            {{ asset.name }}</v-list-item-subtitle
+          >
         </v-list-item-content>
       </v-list-item>
-      <v-list-item v-if="getContactName(tx) != null" three-line class="blue-grey lighten-4">
+      <v-list-item
+        v-if="getContactName(tx) != null"
+        three-line
+        class="blue-grey lighten-4"
+      >
         <v-list-item-content>
-          <v-list-item-title>{{tx | typeFormat2(wallet)}}</v-list-item-title>
-          <v-list-item-subtitle>{{getContactName(tx)}}</v-list-item-subtitle>
-          <v-list-item-subtitle>{{getAddress(tx)}}</v-list-item-subtitle>
+          <v-list-item-title>{{ tx | typeFormat2(wallet) }}</v-list-item-title>
+          <v-list-item-subtitle>{{ getContactName(tx) }}</v-list-item-subtitle>
+          <v-list-item-subtitle>{{ getAddress(tx) }}</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
       <v-list-item v-else two-line class="blue-grey lighten-4">
         <v-list-item-content>
-          <v-list-item-title>{{tx | typeFormat2(wallet)}}</v-list-item-title>
-          <v-list-item-subtitle>{{getAddress(tx)}}</v-list-item-subtitle>
+          <v-list-item-title>{{ tx | typeFormat2(wallet) }}</v-list-item-title>
+          <v-list-item-subtitle>{{ getAddress(tx) }}</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
       <v-list-item two-line>
         <v-list-item-content>
           <v-list-item-title>Дата</v-list-item-title>
-          <v-list-item-subtitle>{{tx.timestamp | dateFormat}}</v-list-item-subtitle>
+          <v-list-item-subtitle>{{
+            tx.timestamp | dateFormat
+          }}</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
       <v-list-item two-line>
         <v-list-item-content>
           <v-list-item-title>Статус</v-list-item-title>
-          <v-list-item-subtitle :class="statusClass">{{tx.applicationStatus | statusFormat}}</v-list-item-subtitle>
+          <v-list-item-subtitle :class="statusClass">{{
+            tx.applicationStatus | statusFormat
+          }}</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
     </v-list>
@@ -54,11 +73,8 @@ export default {
     statusClass() {
       if (this.tx.applicationStatus == "succeeded") {
         return { "success--text": true };
-      // TODO: статусы ниже нужно проработать по документации
-      } else if (this.tx.status == "process") {
+      } else if (this.tx.applicationStatus == null) {
         return { "warning--text": true };
-      } else if (this.tx.status == "error") {
-        return { "error--text": true };
       } else {
         return { "error--text": true };
       }
@@ -68,32 +84,44 @@ export default {
   },
   methods: {
     getContactName(tx) {
-      const value = tx.sender == this.wallet.address ? tx.recipient : tx.sender;
+      const value = this.getAddress(tx);
       const name = this.contactName(value);
       return name != null ? name : null;
     },
     getAddress(tx) {
-      return tx.sender == this.wallet.address ? tx.recipient : tx.sender;
+      if (tx.type == 3) return tx.sender;
+      if (tx.type == 4) return tx.sender == this.wallet.address ? tx.recipient : tx.sender;
     },
   },
   filters: {
     typeFormat(tx, wallet) {
-      if (tx.sender == wallet.address) {
-        return "Отправлено";
-      } else {
-        return "Получено";
+      if (tx.type == 3) {
+        return "Создано";
+      } else if (tx.type == 4) {
+        if (tx.sender == wallet.address) {
+          return "Отправлено";
+        } else {
+          return "Получено";
+        }
       }
     },
     amountFormat(tx, wallet) {
-      if (tx.type == 3) return `+${tx.quantity}`;
-      if (tx.type == 4) return tx.sender == wallet.address ? `-${tx.amount}` : `+${tx.amount}`;
+      if (tx.type == 3) return `+${Intl.NumberFormat().format(tx.quantity)}`;
+      if (tx.type == 4) {
+        const amount = Intl.NumberFormat().format(tx.amount);
+        return tx.sender == wallet.address ? `-${amount}` : `+${amount}`;
+      }
       return 0;
     },
     typeFormat2(tx, wallet) {
-      if (tx.sender == wallet.address) {
-        return "Отправлено";
-      } else {
-        return "Получено от";
+      if (tx.type == 3) {
+        return "Создано";
+      } else if (tx.type == 4) {
+        if (tx.sender == wallet.address) {
+          return "Отправлено";
+        } else {
+          return "Получено от";
+        }
       }
     },
     dateFormat(value) {
@@ -102,15 +130,13 @@ export default {
     statusFormat(value) {
       if (value == "succeeded") {
         return "Выполнено";
-      // TODO: статусы ниже нужно проработать по документации
-      } else if (value == "process") {
-        return "В процессе";
-      } else if (value == "error") {
-        return "Ошибка";
+      } else if (value == null) {
+        // почему-то не везде в транзакциях есть поле со статусом
+        return "Выполнено";
       } else {
-        return "Не известно";
+        return value;
       }
-    }
+    },
   },
 };
 </script>
