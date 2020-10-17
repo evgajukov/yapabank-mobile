09065a9d8a16a08dfd99a7628aca296fdd8715ea
Timur Evgazhukov <evgajukov@gmail.com>
2020-10-17 13:35:09 +0300

    пока убрали раздел с настройками

--- src/pages/layers/MenuLayer.vue
+++ src/pages/layers/MenuLayer.vue
@@ -10,9 +10,9 @@
         <v-list-item-title>О приложении</v-list-item-title>
       </v-list-item>
       <v-divider></v-divider>
-      <v-list-item to="/settings">
+      <!-- <v-list-item to="/settings">
         <v-list-item-title>Настройки</v-list-item-title>
-      </v-list-item>
+      </v-list-item> -->
       <v-list-item to="/signin" @click="logout">
         <v-list-item-title>Выход</v-list-item-title>
       </v-list-item>
